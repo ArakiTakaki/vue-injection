b6fd1769dafb356b44200877a80e44975b21389c
ArakiTakaki <arakitakaki.work@gmail.com>
2022-06-27 22:05:09 +0900

    fix: ストアの内容を適用

---
+++ src/component/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from '@vue/reactivity';
+import useUpdateHelloWorld from '../store/HelloWorld/actions/useUpdateHelloWorld';
+import useGetHelloWorldText from '../store/HelloWorld/selector/useGetHelloWorldText';
+
+const refText = ref('');
+const updateText = useUpdateHelloWorld();
+const text = useGetHelloWorldText();
+const handleSubmit = () => {
+    updateText(refText.value);
+    refText.value = '';
+}
+</script>
+
+<template>
+<div>
+  <div>{{text}}</div>
+  <input type="text" name="" id="" v-model="refText">
+  <button type="button" @click="handleSubmit">更新</button>
+</div>
+</template>
+
+<style>
+</style>
